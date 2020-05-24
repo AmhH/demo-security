14553d98011b09eb2bbe77ddc772b4c00b91d196
Amh <abaams78@gmail.com>
2020-05-23 21:08:31 -0500

    security authorize

--- src/main/java/com/example/SecurityConfiguration.java
+++ src/main/java/com/example/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.example;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -26,7 +29,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        /**
+         * Put most restrictive first and less restrictive on the way down
+         */
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/", "static/css", "static/js").permitAll()
+                .and().formLogin();
     }
 
     @Bean
